096e4edf7cfe23a1213189c1790b401b9f5b7a35
linda775 <linda.lugaresi@galileiostiglia.edu.it>
2025-10-24 09:54:51 +0000

    caramelle

---
+++ caramelle.c
@@ -0,0 +1,9 @@
+//Esercizio 4- Caramelle
+//Dato un numero di caramelle da dividere per un certo numero di amici, 
+//che ne devono ricevere tutti lo stesso numero, dire quante caramelle verranno date a testa e quante ne rimangono non distribuite.
+
+#include <stdio.h>
+
+int main(){
+    int caramelle, amici;
+    int a_testa, rimanenti;
